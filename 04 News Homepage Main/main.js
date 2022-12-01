81d069a5c969005b46d06b26c237754410e4e76c
Ivan <ivansam382@gmail.com>
2022-12-01 15:27:54 +0100

    mobile done, working on desktop

--- 04 News Homepage Main/main.js
+++ 04 News Homepage Main/main.js
@@ -2,6 +2,12 @@ const sidebarOpen = document.querySelector("#open");
 const sidebarClose = document.querySelector("#close");
 const navbar = document.querySelector("nav");
 
+if (window.innerWidth > 800) {
+  navbar.classList.remove("hide");
+  sidebarOpen.classList.add("hide");
+  sidebarClose.classList.add("hide");
+}
+
 sidebarOpen.addEventListener("click", open);
 sidebarClose.addEventListener("click", close);
 
